8aad5e84403adab9499d9ece246e00692d83552a
Eugen Winter <eugen.winter@haw-hamburg.de>
2014-10-07 22:53:26 +0200

    7. Oktober 2014, 22:45 - Erste lauffaehige Version.

--- src/rmi/MessageServiceImpl.java
+++ src/rmi/MessageServiceImpl.java
@@ -8,20 +8,24 @@ import java.rmi.server.UnicastRemoteObject;
 public class MessageServiceImpl extends UnicastRemoteObject implements MessageService {
 
     private static final long serialVersionUID = 1L;
+    private final int DELIVERYQUEUESIZE = 10; 
     private DeliveryQueue deliveryQueue;
     
     protected MessageServiceImpl() throws RemoteException {
         super();
+        deliveryQueue = new DeliveryQueue(DELIVERYQUEUESIZE);
     }
 
     @Override
     public String nextMessage(String clientID) throws RemoteException { // synchronized, if using threading
+        System.out.println("Client hat Methode nextMessage aufgerufen\n");
         Message message = deliveryQueue.getMessage(clientID);        
         return message.toString();
     }
 
     @Override
     public void newMessage(String clientID, String message) throws RemoteException { // synchronized, if using threading
+        System.out.println("Client hat Methode newMessage aufgerufen\n");
         Message newMessage = new Message(clientID, message);
         deliveryQueue.addMessage(newMessage);
         System.out.println("Neue Nachricht von Client-ID [" + clientID + "]: " + message + "\n");
